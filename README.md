cd6bb8f3d670b5ff9777fad56624f8bca455e143
Lefteris Chatzipetrou <lefteris@hamsystems.eu>
2017-07-05 20:47:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,104 @@
+Minimalist Library to draw temperature maps (heat maps) using WebGL in pure Javascript. Except a O(N) pre-process step which is done in Javascipt, all calculations and drawing are done with shaders in WebGL, so it is pretty fast. It is also very small (~3kB minified and gzipped)
+
+'OES_texture_float' extension is required.
+
+### Using the library
+
+#### HTML
+```html
+...
+<script type='text/javascript' src='temperature-map-gl.min.js'></script>
+...
+<div class='map-container' style='position:relative;'>
+	<img id='map-image0' src='symi.png'/>
+</div>
+```
+
+#### Javascript
+```js
+var image = document.getElementById("map-image0");
+var temperature_map = new temperature_map_gl(image);
+temperature_map.set_points(points);
+temperature_map.draw();
+```
+
+points are in this format:
+```js
+var points = [
+	[x0,y0,v0],
+	[x1,y1,v1],
+	...
+	[xN,yN,vN]
+]
+```
+
+#### Available options (the defaults are shown)
+```js
+var temperature_map = new temperature_map_gl(image), {
+	p: 3, // used in calculating the IDW values, see wikipedia article mentioned at the bottom of this
+	canvas: null, //use this canvas element and don't create a new one
+	opacity: 0.5,// opacity of the canvas
+	range_factor: 0.01,//used in scaling the values so they don't clip when storing them as channels of the framebuffer texture
+	gamma: 2.2,//used in altering the color during draw pass
+	brightness: 0.00,//used in brightening the color during draw pass
+	contrast: 6,//used in altering the color during draw pass
+	show_points: false,//add 
+	framebuffer_factor: 0.25,//the ratio of the dimensions of the calculation framebuffer in relation to the actual canvas
+	image_zindex: 0,//style z-index given to the image
+	floating_point_texture: true,//use floating point texture for calculation, if not supported this automatically becomes false, but the results are incorrect...
+	point_text: function(val) {//used when the show_points is true of the draw_points() method is called explicitly. It returns the text on the points shown for given value val
+		var v;
+		if(val < 1)
+			v = val.toFixed(2);
+		else if(val < 10)
+			v = val.toFixed(1);
+		else
+			v = Math.round(val);
+		return v + "°C";
+	}
+};
+```
+
+### Methods
+
+```js
+//constructor
+temperature_map_gl(image_element[, options]);
+
+//update some options, not all constructor options will have an effect..
+temperature_map.update_options(options);
+
+//sets points in the format mentioned above
+temperature_map.set_points(points);
+
+//performs a calculation and draw given the points set
+temperature_map.draw();
+
+//explicitly draw markers on points, like using the show_points option
+temperature_map.draw_points()
+
+//explicitly hide markers
+temperature_map.hide_points()
+
+//removes all created elements
+temperature_map.destroy()
+```
+
+### Examples
+
+
+You can also check it out live at [chpetrou.net](http://chpetrou.net/temperature-map-js/)
+
+### Technical explanation
+
+Values are calculated using 'Inverse Distance Weighting (IDW)' algorithm:
+
+[Wikipedia - Inverse Distance Weighting](https://en.wikipedia.org/wiki/Inverse_distance_weighting)
+
+The rest of the explanation makes sense only in the context of the wikipedia article above...
+
+For every point, we perform a render pass to a texture. Using IDW, we calculate the point "influence" to every fragment using a fragment shader. We store the ui*wi at the r channel of the texture and w_i at the g channel. Using blending with "accumulator" configuration, we end end up with a texture, where we have the top sum of IDW in r channel, and the bottom sum at the g channel. Since channels WebGL are clamped in [0,1], we multiply both channels with range_factor to avoid clamping.
+
+At last, we perform a last pass where we get the IDW value by reading the calculation texture and do a r/g at every fragment. We then use this value to determine the color of the fragment. 
+
+More on the technical side on [my website](http://chpetrou.net/en/temperature-map-gl-js-minimalist-pure-javascript-heat-map-library-using-webgl-shaders/)
---
+++ temperature-map-gl.js
@@ -0,0 +1,394 @@
+//! temperature-map-gl.js
+//! version : 0.5.0
+//! authors : Lefteris Chatzipetrou
+//! license : MIT
+//! http://chpetrou.net
+
+;(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    global.temperature_map_gl = factory()
+}(this, (function () { 'use strict';
+
+	var vertex_shader_source = "\
+		attribute vec2 position;														\
+																						\
+		void main(void) {																\
+			gl_Position = vec4(position.x*2.0-1.0, position.y*2.0-1.0, 1.0, 1.0);		\
+		}																				\
+	";
+
+	var computation_fragment_shader_source = "\
+		precision highp float;															\
+																						\
+		uniform float ui;																\
+		uniform vec2 xi;																\
+		uniform float p;																\
+		uniform float range_factor;														\
+		uniform vec2 screen_size;														\
+		void main(void) {																\
+			vec2 x = vec2(gl_FragCoord.x/screen_size.y, gl_FragCoord.y/screen_size.y);	\
+			float dist = distance(x, xi);												\
+			float wi = 1.0/pow(dist, p);												\
+			gl_FragColor = vec4(ui*wi*range_factor, wi*range_factor, 0.0, 1.0);			\
+		}																				\
+	";
+
+	var draw_fragment_shader_source = "\
+		precision highp float;																														\
+																																					\
+		uniform sampler2D computation_texture;																										\
+		uniform vec2 screen_size;																													\
+		uniform float gamma;																														\
+		uniform float brightness;																													\
+		uniform float contrast;																														\
+		void main(void) {																															\
+			vec4 data = texture2D(computation_texture, vec2(gl_FragCoord.x/screen_size.x, 1.0-gl_FragCoord.y/screen_size.y));						\
+			float val = data.x/data.y;																												\
+			vec3 color = vec3(max((val-0.5)*2.0, 0.0), 1.0 - abs(val - 0.5), max((0.5-val)*2.0, 0.0) ) + vec3(brightness, brightness, brightness);	\
+			gl_FragColor.rgb = pow(color, vec3(1.0/gamma));																							\
+			gl_FragColor.a = 1.0;																													\
+		}																																			\
+	";
+
+	function get_shader(gl, source, type){
+		if(type == 'fragment'){
+			type = gl.FRAGMENT_SHADER;
+		}
+		else if(type == 'vertex'){
+			type = gl.VERTEX_SHADER;
+		}
+		else {
+			return null;
+		}
+
+		var shader = gl.createShader(type);
+		gl.shaderSource(shader, source);
+		gl.compileShader(shader);
+
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+			console.log('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+			gl.deleteShader(shader);
+			return null;  
+		}
+			
+		return shader;
+	}
+
+	function get_program(gl, vertex_shader, fragment_shader){
+		var program = gl.createProgram();
+		gl.attachShader(program, vertex_shader);
+		gl.attachShader(program, fragment_shader);
+		gl.linkProgram(program);
+
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
+		}
+
+		return program;
+	}
+	
+	var default_options = {
+		p: 3,
+		canvas: null,
+		opacity: 0.5,
+		range_factor: 0.01,
+		gamma: 2.2,
+		brightness: 0.00,
+		contrast: 6,
+		show_points: false,
+		framebuffer_factor: 0.25,
+		image_zindex: 0,
+		normal_value: 25,
+		floating_point_texture: true,
+		point_text: function(val) {
+			var v;
+			if(val < 1)
+				v = val.toFixed(2);
+			else if(val < 10)
+				v = val.toFixed(1);
+			else
+				v = Math.round(val);
+			return v + "°C";
+		}
+	};
+	
+	var instance = 0;
+	function temperature_map_gl(image_element, options){
+		var _options = {};
+		for(var k in default_options)
+			_options[k] = default_options[k];
+
+		if(typeof options === 'object'){
+			for(var k in options)
+				_options[k] = options[k];
+		}
+
+		this.instance = instance++;
+
+		this.image_element = image_element;
+		image_element.style.zIndex = _options.image_zindex;
+
+		var canvas = _options.canvas || document.createElement('canvas');
+		canvas.height = this.image_element.clientHeight;
+		canvas.width = this.image_element.clientWidth;
+		canvas.style.height = canvas.height+'px';
+		canvas.style.width = canvas.width+'px';
+
+		canvas.style.position = 'absolute';
+		canvas.style.top = '0px';
+		canvas.style.left = '0px';
+		canvas.style.zIndex = image_element.style.zIndex?''+(Number(image_element.style.zIndex)+1):'10';
+		canvas.style.opacity = _options.opacity;
+		if(!_options.canvas){
+			image_element.parentNode.insertBefore(canvas, image_element.nextSibling);
+			this.own_canvas = true;
+		}
+		else
+			this.own_canvas = false;
+		this.canvas = canvas;
+		
+		this.context = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
+		if(!this.context)
+			console.log('Your browser does not support webgl');
+		if(!this.context.getExtension('OES_texture_float')){
+			console.log('Your browser does not support float textures');
+			_options.floating_point_texture = false;
+			_options.range_factor = 0.01;
+		}
+
+		this.points = [];
+		this.translated_points = [];
+		this.square_vertices_buffer = null;
+		this.computation_program = null;
+		this.draw_program = null;
+		this.position_attribute = null;
+		this.computation_framebuffer = null;
+		this.computation_texture = null;
+		
+		this.ui_uniform = null;
+		this.xi_uniform = null;
+		this.c_screen_size_uniform = null;
+		this.d_screen_size_uniform = null;
+		this.range_factor_uniform = null;
+		this.p_uniform = null;
+		this.computation_texture_uniform = null;
+		this.gamma_uniform = null;
+		this.brightness_uniform = null;
+		this.contrast_uniform = null;
+
+
+		this.point_text = _options.point_text;
+		this.p = _options.p;
+		this.range_factor = _options.range_factor;
+		this.gamma = _options.gamma;
+		this.brightness = _options.brightness;
+		this.contrast = _options.contrast;
+
+		this.show_points = _options.show_points;
+		this.unit = _options.unit;
+
+		this.framebuffer_factor = _options.framebuffer_factor;
+		this.floating_point_texture = _options.floating_point_texture;
+		this.computation_framebuffer_width = 0;
+		this.computation_framebuffer_height = 0;
+		
+		
+		this.init_buffers();
+		this.init_shaders();
+	}
+
+	temperature_map_gl.prototype.update_options = function(options){
+		if(options.p) this.p = options.p;
+		if(options.range_factor) this.range_factor = options.range_factor;
+		if(options.gamma) this.gamma = options.gamma;
+		if(options.brightness) this.brightness = options.brightness;
+		if(options.contrast) this.contrast = options.contrast;
+		if(options.show_points) this.show_points = options.show_points;
+
+		this.draw();
+	}
+
+	temperature_map_gl.prototype.set_points = function(points){
+		this.points = points;
+		if(points.length){
+			var translated_points = [];
+			var min = points[0][2];
+			var max = points[0][2];
+			for(var i=1;i < points.length; ++i){
+				var p = [points[i][0], points[i][1], points[i][2]];
+				if(p[2] > max)
+					max = p[2];
+				if(p[2] < min)
+					min = p[2];
+			}	
+			var d = max > min?max - min:1;
+
+			var w = this.canvas.width;
+			var h = this.canvas.height;
+			for(var i=0;i < points.length; ++i){
+				var p = [points[i][0], points[i][1], points[i][2]];
+				p[0] = p[0]/w;
+				p[1] = p[1]/h;
+				p[2] = (p[2] - min)/d;
+				
+				
+				if(p[2] > max)
+					max = p[2];
+				if(p[2] < min)
+					min = p[2];
+
+				translated_points.push(p);
+			}
+			this.translated_points = translated_points;
+		}
+	}
+
+
+	temperature_map_gl.prototype.init_buffers = function(){
+		var gl = this.context;
+		this.square_vertices_buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.square_vertices_buffer);
+		
+		var vertices = [
+			1.0,  1.0,
+			-1.0, 1.0,
+			1.0,  -1.0,
+			-1.0, -1.0
+		];
+		
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+		this.computation_framebuffer_width = Math.ceil(this.canvas.width*this.framebuffer_factor);
+		this.computation_framebuffer_height = Math.ceil(this.canvas.height*this.framebuffer_factor);
+
+		this.computation_texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, this.computation_texture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.computation_framebuffer_width, this.computation_framebuffer_height, 0, gl.RGBA, this.floating_point_texture?gl.FLOAT:gl.UNSIGNED_BYTE, null);
+		
+		this.computation_framebuffer = gl.createFramebuffer();
+		gl.bindFramebuffer(gl.FRAMEBUFFER, this.computation_framebuffer);
+		gl.viewport(0, 0, this.computation_framebuffer_width, this.computation_framebuffer_height);
+		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.computation_texture, 0);
+
+		gl.bindTexture(gl.TEXTURE_2D, null);
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+	}
+
+
+	temperature_map_gl.prototype.init_shaders = function(){
+		var gl = this.context;
+		var vertex_shader = get_shader(gl, vertex_shader_source, 'vertex');
+		var computation_fragment_shader = get_shader(gl, computation_fragment_shader_source, 'fragment');
+		var draw_fragment_shader = get_shader(gl, draw_fragment_shader_source, 'fragment');
+
+		this.computation_program = get_program(gl, vertex_shader, computation_fragment_shader);
+		this.position_attribute = gl.getAttribLocation(this.computation_program, 'position');
+		this.ui_uniform = gl.getUniformLocation(this.computation_program, "ui");
+		this.xi_uniform = gl.getUniformLocation(this.computation_program, "xi");
+		this.c_screen_size_uniform = gl.getUniformLocation(this.computation_program, "screen_size");
+		this.range_factor_uniform = gl.getUniformLocation(this.computation_program, "range_factor");
+		this.p_uniform = gl.getUniformLocation(this.computation_program, "p");
+		gl.enableVertexAttribArray(this.position_attribute);
+
+		this.draw_program = get_program(gl, vertex_shader, draw_fragment_shader);
+		this.d_screen_size_uniform = gl.getUniformLocation(this.draw_program, "screen_size");
+		this.computation_texture_uniform = gl.getUniformLocation(this.draw_program, 'computation_texture');
+		this.gamma_uniform = gl.getUniformLocation(this.draw_program, 'gamma');
+		this.brightness_uniform = gl.getUniformLocation(this.draw_program, 'brightness');
+	}
+
+
+	temperature_map_gl.prototype.draw = function(){
+		if(!this.context)
+			return;
+
+		var gl = this.context;
+		
+		gl.disable(gl.DEPTH_TEST);
+		
+		gl.enable(gl.BLEND);
+		gl.blendEquation(gl.FUNC_ADD);
+		gl.blendFunc(gl.ONE, gl.ONE);
+		
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+		gl.useProgram(this.computation_program);
+		gl.uniform2f(this.c_screen_size_uniform, this.computation_framebuffer_width, this.computation_framebuffer_height);
+		gl.uniform1f(this.p_uniform, this.p);
+		gl.uniform1f(this.range_factor_uniform, this.range_factor);
+		gl.bindFramebuffer(gl.FRAMEBUFFER, this.computation_framebuffer);
+		gl.viewport(0, 0, this.computation_framebuffer_width, this.computation_framebuffer_height);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		
+		for(var i=0; i < this.translated_points.length; ++i){
+			var p = this.translated_points[i];
+			gl.uniform2f(this.xi_uniform, p[0], p[1]);
+			gl.uniform1f(this.ui_uniform, p[2]);
+
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.square_vertices_buffer);
+			gl.vertexAttribPointer(this.position_attribute, 2, gl.FLOAT, false, 0, 0);
+			gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+		}
+		
+		
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		gl.useProgram(this.draw_program);
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, this.computation_texture);
+		gl.uniform1i(this.computation_texture_uniform, 0);
+		gl.uniform1f(this.gamma_uniform, this.gamma);
+		gl.uniform1f(this.brightness_uniform, this.brightness);
+		gl.uniform1f(this.contrast_uniform, this.contrast);
+		gl.uniform2f(this.d_screen_size_uniform, this.canvas.width, this.canvas.height);
+		gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.square_vertices_buffer);
+		gl.vertexAttribPointer(this.position_attribute, 2, gl.FLOAT, false, 0, 0);
+
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+		if(this.show_points)
+			this.draw_points();
+		else
+			this.hide_points();
+	}
+	
+	temperature_map_gl.prototype.hide_points = function(){
+		var elements = document.getElementsByClassName('tmg-point-'+this.instance);
+		while(elements[0]) {
+    		elements[0].parentNode.removeChild(elements[0]);
+		}
+	}
+
+	temperature_map_gl.prototype.draw_points = function(){
+		this.hide_points();
+
+		for(var i=0; i < this.points.length; ++i){
+			var p = this.points[i];
+			var dot = document.createElement('p');
+			dot.style.position = 'absolute';
+			dot.style.left = p[0]+'px';
+			dot.style.top = p[1]+'px';
+			dot.style.zIndex = ''+(Number(this.canvas.style.zIndex)+1);
+			dot.className = 'tmg-point '+'tmg-point-'+this.instance;
+			dot.innerHTML = this.point_text(p[2]);
+			this.canvas.parentNode.insertBefore(dot, this.canvas.nextSibling);
+		}
+	}
+
+	temperature_map_gl.prototype.destroy = function(){
+		if(this.own_canvas)
+			this.canvas.parentNode.removeChild(this.canvas);
+		this.hide_points();
+	}
+
+	return temperature_map_gl;
+})));
